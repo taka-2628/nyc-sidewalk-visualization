1e9095447e654d37881f66bf25545bfe2ab8d908
Takashi <th2826@columbia.edu>
2022-08-27 21:14:17 -0400

    basic map view created

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,45 @@
-import logo from '../logo.svg';
+import React, { useRef, useEffect, useState } from 'react';
 import '../stylesheets/App.css';
 
+import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+ 
+mapboxgl.accessToken = 'pk.eyJ1IjoidGgtdGgiLCJhIjoiY2t3N2Q1YmNxOW8wajMxczE4ZndqaDRuNCJ9.UWfb1rN9Hl6lBXJGLC6Vrw';
+
 function App() {
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [lng, setLng] = useState(-73.95);
+  const [lat, setLat] = useState(40.78);
+  const [zoom, setZoom] = useState(11);  
+  
+  useEffect(() => {
+    if (map.current) return; // initialize map only once
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: 'mapbox://styles/th-th/cl7571mfr001l14pim5etz43v',
+      center: [lng, lat],
+      zoom: zoom
+    });
+  });
+     
+  useEffect(() => {
+    if (!map.current) return; // wait for map to initialize
+    map.current.on('move', () => {
+      setLng(map.current.getCenter().lng.toFixed(4));
+      setLat(map.current.getCenter().lat.toFixed(4));
+      setZoom(map.current.getZoom().toFixed(2));
+    });
+  });
+     
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="sidebar">
+        Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+        <div id="slidebar">
+          
+        </div>
+      </div>
+      <div ref={mapContainer} className="map-container" />
     </div>
   );
 }
